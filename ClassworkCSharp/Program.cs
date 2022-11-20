7b3c3612468333cc4704fe2da3eeb33f7f651da2
Lev_Sychev <mrdiamondlev@gmail.com>
2022-11-20 18:11:51 +0500

    Добавьте файлы проекта.

---
+++ ClassworkCSharp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ClassworkCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: вынести в отдельную функцию
+            FileStream file = Task1CreateFile("Day17_VERY_WIP");
+            string aaaa = "I don't know what I'm doing";
+
+            foreach (char c in aaaa)
+                file.WriteByte(((byte)c));
+            file.Close();
+
+            Task1ReadFile("Day17_VERY_WIP");
+            Console.ReadKey();
+        }
+
+        private static FileStream Task1CreateFile(string filename)
+        {
+            FileStream trytoreadfile;
+
+            try
+            {
+                trytoreadfile = new FileStream($"{filename}.txt", FileMode.Open);
+            }
+            catch (FileNotFoundException)
+            {
+                FileStream fs = new FileStream($"{filename}.txt", FileMode.Create);
+                return fs;
+            }
+            Console.WriteLine("File already exists");
+
+            return trytoreadfile;
+        }
+        private static void Task1ReadFile(string filename)
+        {
+            using (FileStream fs = new FileStream($"{filename}.txt", FileMode.Open))
+            using (BinaryReader file = new BinaryReader(fs, Encoding.UTF8))
+            {
+                foreach (char c in file.ReadBytes((int)fs.Length))
+                    Console.Write(c);
+            }
+
+        }
+        private static void Task1WriteIntoFile(string filename)
+        {
+
+        }
+    }
